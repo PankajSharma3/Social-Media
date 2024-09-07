72ba49ec08d033e2eebf0a449b55511218c8d590
Pankaj <ps90798173@gmail.com>
2024-09-07 22:06:09 +0530

    backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -21,6 +22,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json({ limit: "5mb" })); 
 app.use(express.urlencoded({ extended: true })); 
 
